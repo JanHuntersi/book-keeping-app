c3eeb8bd44b8f043a1c5fd8bff2115a151531707
Jan Sernec <jan.sernec@gmail.com>
2021-10-19 00:13:52 +0200

    UpdateProgress

--- src/components/UpdateProgressModal.js
+++ src/components/UpdateProgressModal.js
@@ -1,15 +1,16 @@
-export default function UpdateProgressModal({show,handleClose}){
-    return(
-        
-        <div className="modal" style={{display: show ? 'block':'none'}}>
-           
-  <div class="modal-content">
-    <span onClick={handleClose} class="close">&times;</span>
-    <h1>Update Chapter</h1>
+export default function UpdateProgressModal({ show, handleClose }) {
+    return (
 
-    <p>New chapter:</p>
-    
-  </div>
+        <div className="modal" style={{ display: show ? 'block' : 'none' }}>
+
+            <div class="modal-content">
+                <span onClick={handleClose} class="close">&times;</span>
+                <h1>Update Chapter</h1>
+                <p>New chapter:</p>
+                <p>test</p>
+            </div>
         </div>
     )
 }
+
+//Add functionality to update chapter
